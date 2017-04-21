83c3ab2006c55ef74f9d7bd9fd2234db1cbf94c7
OCram85 <marco.blessing@googlemail.com>
2017-04-21 07:32:38 +0200

    adds AppVeyor Support; fixes #1

--- src/Private/Get-GitInfo.ps1
+++ src/Private/Get-GitInfo.ps1
@@ -4,18 +4,33 @@ Function Get-GitInfo () {
         [String]$BranchName
     )
 
-    If (-not $BranchName) {
-        $BranchName = (git rev-parse --abbrev-ref HEAD)
-    }
-    Return [PSCustomObject]@{
-        head = [PSCustomObject]@{
-            id = (git log --format="%H" HEAD -1)
-            author_name = (git log --format="%an" HEAD -1)
-            author_email = (git log --format="%ae" HEAD -1)
-            committer_name = (git log --format="%cn" HEAD -1)
-            committer_email = (git log --format="%ce" HEAD -1)
-            message = (git log --format="%s" HEAD -1)
+    If ($Env:AppVeyor) {
+        Return [PSCustomObject]@{
+            head = [PSCustomObject]@{
+                id = $Env:APPVEYOR_REPO_COMMIT
+                author_name = $Env:APPVEYOR_REPO_COMMIT_AUTHOR
+                author_email = $Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
+                commiter_name = $Env:APPVEYOR_REPO_COMMIT_AUTHOR
+                commiter_email = $Env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
+                message = $Env:APPVEYOR_REPO_COMMIT_MESSAGE
+            }
+            branch = $Env:APPVEYOR_REPO_BRANCH
         }
-        branch = $BranchName
+    }
+    Else {
+        If (-not $BranchName) {
+            $BranchName = (git rev-parse --abbrev-ref HEAD)
+        }
+        Return [PSCustomObject]@{
+            head = [PSCustomObject]@{
+                id = (git log --format="%H" HEAD -1)
+                author_name = (git log --format="%an" HEAD -1)
+                author_email = (git log --format="%ae" HEAD -1)
+                committer_name = (git log --format="%cn" HEAD -1)
+                committer_email = (git log --format="%ce" HEAD -1)
+                message = (git log --format="%s" HEAD -1)
+            }
+            branch = $BranchName
+        }
     }
 }
