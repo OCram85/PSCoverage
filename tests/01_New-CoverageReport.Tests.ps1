2bf1f6af2ccf3bba7065c0570f2a4e8b1dcab698
OCram85 <marco.blessing@googlemail.com>
2019-01-17 10:22:20 +0100

    fix Pester test

--- tests/01_New-CoverageReport.Tests.ps1
+++ tests/01_New-CoverageReport.Tests.ps1
@@ -8,12 +8,12 @@ $TestModuleTests = Join-Path -Path $RepoRoot -ChildPath '/resources/TestModule/T
 Describe 'New-CoverageReport' {
     Context 'Basic tests' {
         It 'Test1: Should not throw' {
-            $TestResults = Invoke-Pester -Path $TestModuleTests -CodeCoverage $TestModuleSrc -PassThru -Show None
+            $TestResults = Invoke-Pester -Path $TestModuleTests -CodeCoverage $TestModuleSrc -PassThru -Show None -ErrorAction SilentlyContinue
             { $CoverageReport = New-CoverageReport -CodeCoverage $TestResults.CodeCoverage -RepoToken '123456'} | Should -Not -Throw
         }
-    }
-    Context 'Coverage report validation' {
         It 'Test2: Source file count should be 3' {
+            $TestResults = Invoke-Pester -Path $TestModuleTests -CodeCoverage $TestModuleSrc -PassThru -Show None -ErrorAction SilentlyContinue
+            $CoverageReport = New-CoverageReport -CodeCoverage $TestResults.CodeCoverage -RepoToken '123456'
             $CoverageReport.source_files.Count | Should -Be 3
         }
     }
